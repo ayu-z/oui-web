ffd99fe50904f035456792ea0aef3d3c499ab078
ayu-z <yuzuihong@live.com>
2023-11-20 11:23:58 +0000

    fix iface page

--- applications/oui-app-demo/htdoc/index.vue
+++ applications/oui-app-demo/htdoc/index.vue
@@ -1,6 +1,54 @@
 <template>
-  <h1>Oui App Demo</h1>
-  <div>{{ $t('hello') }}</div>
+  <div>
+    <n-h2>{{ $t('InterFace Mode') }}</n-h2>
+    <n-form size="large" ref="form" label-placement="left" label-width="80px" :model="formValue" :rules="rules">
+      <n-form-item :label="$t('Mode')" path="mode">
+        <!-- Using Naive UI n-select -->
+        <n-select :modelValue="formValue.mode" :options="modeOptions" filterable @update:value="handleModeChange" />
+      </n-form-item>
+    </n-form>
+  </div>
 </template>
 
-<i18n src="./locale.json"/>
+<script>
+export default {
+  data() {
+    return {
+      formValue: {
+        mode: '' // Set the default value here
+        // ... other form fields
+      },
+      modeOptions: ['1-WAN N-LAN', 'Full-LAN'],
+      showModeDropdown: false // Initially hide the dropdown
+      // ... other data properties
+    }
+  },
+  methods: {
+    handleModeChange(value) {
+      // Handle mode change if needed
+      this.formValue.mode = value
+    },
+    fetchDataAndSetVisibility() {
+      this.$oui
+        .ubus('uci', 'get', { 'config': 'network', 'section': '@switch_vlan[1]' })
+        .then((response) => {
+          // const ports = response.values.ports
+          // Update the visibility of the dropdown based on the condition
+          // this.showModeDropdown = ports !== '4 5'
+          // if (this.showModeDropdown) {
+          // If the dropdown is visible, set the default value
+          this.formValue.mode = response.values.ports
+          // }
+        })
+        .catch((error) => {
+          // Handle error if needed
+          console.log(error)
+        })
+    }
+  },
+  created() {
+    // Call the method to fetch data and set the visibility
+    this.fetchDataAndSetVisibility()
+  }
+}
+</script>
--- applications/oui-app-iface/htdoc/index.vue
+++ applications/oui-app-iface/htdoc/index.vue
@@ -2,20 +2,44 @@
   <div>
     <div>
       <n-h2>{{ $t('InterFace Mode') }}</n-h2>
-      <n-form size="large" ref="form" label-placement="left" label-width="auto" :model="formValue" :rules="rules">
+      <n-form size="large" ref="form" label-placement="left" label-width="80px" :model="formValue" :rules="rules">
         <n-form-item :label="$t('Mode')" path="mode">
-          <n-select v-model="formValue.mode" :options="transmode" filterable @change="handlemodeChange" />
+          <n-select v-model="formValue.mode" :options="ifaceTransmode" filterable @update:value="handlemodeChange" :defaultValue="defaultMode" />
         </n-form-item>
       </n-form>
     </div>
 
     <div v-if="showWANSetting">
       <n-h2>{{ $t('WAN Setting') }}</n-h2>
+      <n-form size="large" ref="form" label-placement="left" label-width="80px" :model="formValue" :rules="rules">
+        <n-form-item :label="$t('Mode')" path="wan">
+          <n-select v-model="formValue.wan.proto" :options="wanmodeTransmode" filterable  @update:value="handlewanmodeChange" />
+        </n-form-item>
+        <div v-if="showStaticSetting">
+          <n-form-item :label="$t('IPAddr')" path="wan">
+            <n-input v-model:value="formValue.wan.static.ipaddr"/>
+          </n-form-item>
+          <n-form-item :label="$t('NetMask')" path="wan">
+            <n-input v-model:value="formValue.wan.static.netmask"/>
+          </n-form-item>
+          <n-form-item :label="$t('GateWay')" path="wan">
+            <n-input v-model:value="formValue.wan.static.gateway"/>
+          </n-form-item>
+        </div>
+        <div v-if="showPPPOESetting">
+          <n-form-item :label="$t('UserName')" path="wan">
+            <n-input v-model:value="formValue.wan.pppoe.username"/>
+          </n-form-item>
+          <n-form-item :label="$t('Password')" path="wan">
+            <n-input v-model:value="formValue.wan.pppoe.passwd"/>
+          </n-form-item>
+        </div>
+      </n-form>
     </div>
 
     <div>
       <n-h2>{{ $t('LAN Setting') }}</n-h2>
-      <n-form size="large" ref="form" label-placement="left" label-width="auto" :model="formValue" :rules="rules">
+      <n-form size="large" ref="form" label-placement="left" label-width="80px" :model="formValue" :rules="rules">
       <n-form-item :label="$t('IPAddr')" path="lan">
         <n-input v-model:value="formValue.lan.ipaddr"/>
       </n-form-item>
@@ -33,8 +57,6 @@
   </div>
 </template>
 
-
-
 <script>
 export default {
   data() {
@@ -42,13 +64,14 @@ export default {
       formValue: {
         mode: '',
         wan: {
-          mode: '',
+          proto: '',
           pppoe :{
-            user:'',
+            username:'',
             passwd:''
           },
           static :{
             ipaddr:'',
+            netmask:'',
             gateway:''
           }
         },
@@ -57,8 +80,13 @@ export default {
           netmask: ''
         }
       },
+      defaultMode:'1-WAN N-LAN',
       mode: ['1-WAN N-LAN', 'Full-LAN'],
+      wanmode: ['DHCP', 'Static', 'PPPOE'],
       showWANSetting: false,
+      showStaticSetting: false,
+      showDHCPSetting: false,
+      showPPPOESetting: false,
       rules: {
         // 'lan.ipaddr': [
         //   { required: true, message: this.$t('IP Address is required'), trigger: 'blur' },
@@ -80,26 +108,91 @@ export default {
     }
   },
   computed: {
-    transmode() {
+    ifaceTransmode() {
       return this.mode.map((option) => ({
         value: option,
         label: this.$t(option)
       }))
+    },
+    wanmodeTransmode() {
+      return this.wanmode.map((option) => ({
+        value: option,
+        label: this.$t(option)
+      }))
     }
+
   },
   created() {
+    this.defaultMode = '1-WAN N-LAN'
+    let default_mode = '1-WAN N-LAN'
+    this.$oui.ubus('uci', 'get', { 'config': 'network', 'section': '@switch_vlan[1]' })
+      .then(r => {
+        if (r.values.ports === '4 5') {
+          default_mode = '1-WAN N-LAN'
+        } else {
+          default_mode = 'Full-LAN'
+        }
+      }),
+    this.defaultMode = default_mode,
+    this.handlemodeChange(default_mode),
     this.$oui.ubus('uci', 'get', { 'config': 'network', 'section': 'lan' })
       .then(r => {
         this.formValue.lan.ipaddr = r.values.ipaddr,
         this.formValue.lan.netmask = r.values.netmask
+      }),
+    this.$oui.ubus('uci', 'get', { 'config': 'network', 'section': 'wan' })
+      .then(r => {
+        this.formValue.wan.proto = r.values.proto,
+        this.formValue.wan.static.ipaddr = r.values.ipaddr,
+        this.formValue.wan.static.netmask = r.values.netmask,
+        this.formValue.wan.static.gateway = r.values.gateway,
+        this.formValue.wan.pppoe.username = r.values.username,
+        this.formValue.wan.pppoe.passwd = r.values.password
       })
   },
   methods: {
-    handlemodeChange(selectedOption) {
-      this.showWANSetting = (selectedOption === '1-WAN N-LAN')
+    handlemodeChange(select) {
+      this.showWANSetting = (select === '1-WAN N-LAN')
+      this.defaultMode = select
+    },
+    handlewanmodeChange(select) {
+      this.showStaticSetting = (select === 'Static')
+      this.showPPPOESetting = (select === 'PPPOE')
+      this.formValue.wan.proto = select
     },
     handleSubmit() {
-      // Add your form submission logic here
+      this.$refs.form.validate(async errors => {
+        if (errors)
+          return
+
+        this.loading = true
+
+        console.log('Selected option:', this.formValue.wan.proto, this.formValue.wan.pppoe.username, this.formValue.wan.pppoe.passwd)
+        let proto = this.formValue.wan.proto
+        let ipaddr = this.formValue.wan.static.ipaddr
+        let netmask = this.formValue.wan.static.netmask
+        let gateway = this.formValue.wan.static.gateway
+        let username = this.formValue.wan.pppoe.username
+        let password = this.formValue.wan.pppoe.passwd
+        await this.$oui.call('uci', 'set', {
+          config: 'network',
+          section: 'wan',
+          values: { proto, ipaddr, netmask, gateway, username, password}
+        })
+
+        ipaddr = this.formValue.lan.ipaddr
+        netmask = this.formValue.lan.netmask
+        await this.$oui.call('uci', 'set', {
+          config: 'network',
+          section: 'lan',
+          values: { ipaddr, netmask}
+        })
+        await this.$oui.reloadConfig('network')
+
+        this.loading = false
+
+        this.$message.success(this.$t('Configuration has been applied'))
+      })
     }
   }
 }
